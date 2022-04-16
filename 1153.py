2fb20a5ac519937adf9bf1f734902ddaa7c04db0
André Raye <103725287+AndreRaye@users.noreply.github.com>
2022-04-16 19:44:19 -0400

    1153

---
+++ 1153.py
@@ -0,0 +1,15 @@
+def fatorial():
+    n = int(input())
+    while n <= 0 or n >= 13:
+        n = int(input())
+    
+    fat = 1
+    i = 2
+    
+    while i <= n:
+        fat = fat*i
+        i = i + 1
+
+    print(fat)
+
+fatorial()
