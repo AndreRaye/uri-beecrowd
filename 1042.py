8e3236f8c86b3979b83e7079ee656b57b5702fb8
André Raye <103725287+AndreRaye@users.noreply.github.com>
2022-04-16 19:19:29 -0400

    iniciante

---
+++ 1042.py
@@ -0,0 +1,16 @@
+strA, strB, strC = input().split()
+
+A = int(strA)
+B = int(strB)
+C = int(strC)
+
+Numbers = [A,B,C] 
+
+Numbers.sort()
+print(Numbers[0])
+print(Numbers[1])
+print(Numbers[2])
+print("")
+print(A)
+print(B)
+print(C)
